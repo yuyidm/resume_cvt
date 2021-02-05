36633b67e9d45cc488b78ef0250efe23ad66b168
pengcheng <pengcheng@medpeer.cn>
2021-02-05 21:03:22 +0800

    init

--- app/config/index.js
+++ app/config/index.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 
-// 简历模板名称
-module.exports.CVT_DIR_NAME = '';
-// 是否创建简历模板新版本
-module.exports.CVT_DIR_VERSION = false;
+const TYPE = ['single', 'double', 'three']
+const CVT_DIR_NAME = TYPE[0]; // 简历模板类型
+const CVT_DIR_VERSION = false; // 是否创建简历模板新版本
+
 
 /**
  * 创建模板
--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ const rename = require('gulp-rename'); // js后缀
 const clean = require('gulp-clean'); // 清理文件或文件夹
 
 // 获取配置文件
-const { createTemplate } = require('./config.js');
+const { createTemplate } = require('./app');
 
 /**·
  * sass编译
